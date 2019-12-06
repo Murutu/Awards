3fdff698e743442c3b9df2a53d0d406ee3d020f6
Murutu <murutupeter@email.com>
2019-12-06 15:30:57 +0300

    Added heading

---
+++ Desktop/Git1/Gitsearch/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GitComponent } from './git/git.component';
+import { ServicesComponent } from './services/services.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GitComponent,
+    ServicesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Desktop/Git1/Gitsearch/src/app/github.service.ts.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Githubservice {
+   private username = 'murutu';
+   private client_id = '15549cb3355bb1c179ca';
+   private client_secret = '33659006c550060ede856b764408eecb553e803b';
+
+   constructor() { }
+  }
+  
+  
+
+
