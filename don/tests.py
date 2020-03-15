60eb41fee6407fb52191cbc488d99600baf413d7
Murutu <murutupeter@email.com>
2020-03-15 21:59:33 +0300

    created def setUp in test.py

--- don/tests.py
+++ don/tests.py
@@ -4,5 +4,9 @@ from .models import Project
 # Create your tests here.
 
 class ProjectTestClass(TestCase):
+    def setUp(self):
+        self.newuser=User(username='peter')
+        self.newuser.save()
+        self.projects=Project(title='proj',image='img.jpg',description='fullstackdeveloper',editor=self.newuser)
     
 
