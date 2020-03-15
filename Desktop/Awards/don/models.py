501b8a8c9d326254a6f15cec9b022764b4683d21
Murutu <murutupeter@email.com>
2020-03-15 16:14:05 +0300

    added class Profile in models.py

--- Desktop/Awards/don/models.py
+++ Desktop/Awards/don/models.py
@@ -2,3 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='photos/',null=True)
+    bio = models.CharField(max_length=500)
+    contact=models.CharField(max_length=30)
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    def save_profile(self):
+        self.save()
