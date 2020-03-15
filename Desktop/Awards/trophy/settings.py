f5ccc565e290b22ec57629e7e21844972f8c07d7
Murutu <murutupeter@email.com>
2020-03-15 16:50:59 +0300

    added tinymce in settings.py

--- Desktop/Awards/trophy/settings.py
+++ Desktop/Awards/trophy/settings.py
@@ -31,15 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'bootstrap3',
+    'don',
+    'tinymce',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'bootstrap3',
-    'don',
+    
 ]
 
 MIDDLEWARE = [
