0918edd86bd215dc8f0e95056f14f7d8f49c57b9
Murutu <murutupeter@email.com>
2020-03-15 17:59:18 +0300

    added importObjectDoeNotExist in views.py

--- Desktop/Awards/don/views.py
+++ Desktop/Awards/don/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from .forms import NewProjectForm,NewRatingForm,NewProfileForm
+from django.core.exceptions import  ObjectDoesNotExist
 from .models import Profile,Project,Rating
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
