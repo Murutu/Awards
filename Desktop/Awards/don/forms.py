3553beaf78024f0d2c1f158715dce693f96cef3a
Murutu <murutupeter@email.com>
2020-03-15 17:55:19 +0300

    added class NewProfileForm in forms.py

--- Desktop/Awards/don/forms.py
+++ Desktop/Awards/don/forms.py
@@ -1,2 +1,23 @@
 from django import forms
 from .models import Profile,Project,Rating
+
+class NewProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['editor','profile','pub_date']
+        widgets = {
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+        
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model= Profile
+        exclude=['editor']
+        widgets={
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+        
+class NewRatingForm(forms.ModelForm):
+    class Meta:
+        model=Rating
+        
