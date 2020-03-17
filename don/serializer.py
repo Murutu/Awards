e37e56dc1c2fa5ea66b0d7b8c9a8f5a0609cccb0
Murutu <murutupeter@email.com>
2020-03-17 17:07:14 +0300

    created class ProjectSerializer in serializer.py

--- don/serializer.py
+++ don/serializer.py
@@ -5,3 +5,8 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('profile_photo','bio','contact','editor')
+        
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('title', 'image', 'description', 'link', 'editor', 'profile', 'pub_date')
